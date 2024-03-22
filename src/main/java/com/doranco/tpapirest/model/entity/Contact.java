d59c8f5e393f90ed41a3399c4f18fa148f121283
Ludovic Anselin <lanselin@atakama-technologies.com>
2024-03-22 10:07:37 +0100

    add: Contact entity

---
+++ src/main/java/com/doranco/tpapirest/model/entity/Contact.java
@@ -0,0 +1,77 @@
+package com.doranco.tpapirest.model.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "contact")
+public class Contact {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "firstname", length = 30, nullable = false)
+    private String firstname;
+    @Column(name = "lastname", length = 30, nullable = false)
+    private String lastname;
+    @Column(name = "phone_number", length = 20, nullable = false)
+    private String phoneNumber;
+    @Column(name = "email", length = 100, nullable = false, unique = true)
+    private String email;
+
+    public Contact() {
+
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+}
